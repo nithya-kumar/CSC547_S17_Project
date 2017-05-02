ee2e4b2821a204ea24fd541b9b4c5ab7def551c5
Rushikesh Ghatpande <rsghatpa@ncsu.edu>
2017-05-02 13:36:02 -0700

    Working SOLR installation

---
+++ MeganScripts/cassandra_install.sh
@@ -0,0 +1,3 @@
+cd /opt/
+wget https://archive.apache.org/dist/cassandra/2.2.8/apache-cassandra-2.2.8-bin.tar.gz
+tar -xvzf apache-cassandra-2.2.8-bin.tar.gz
--- MeganScripts/solr-lucene_installation.sh
+++ MeganScripts/solr-lucene_installation.sh
@@ -1,20 +1,19 @@
 cd /opt
-wget https://github.com/apache/lucene-solr/archive/releases/lucene-solr/6.3.0.tar.gz
-mv 6.3.0.tar.gz solr-lucene-6.3.0.tar.gz
-tar -xvzf solr-lucene-6.3.0.tar.gz
-ln -s lucene-solr-releases-lucene-solr-6.3.0/ solr-lucene
+wget http://archive.apache.org/dist/lucene/solr/6.3.0/solr-6.3.0-src.tgz
 
-useradd solr
+tar -xvzf solr-6.3.0-src.tgz 
+ln -s /opt/solr-6.3.0 solr-lucene
+
+sudo adduser solr
+
 chown -R solr:solr /opt/solr-lucene
 sudo -iu solr
 
 cd /opt/solr-lucene
 
 #Need to check the jdk path
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64/
 
-#Compilation problem because tools.jar not found in java 1.8 but if we keep as java 1.7, Solr demands min version 1.8.
-#If this is resolved, foll. steps should work
 /opt/ant/bin/ant ivy-bootstrap
 /opt/ant/bin/ant compile
 /opt/ant/bin/ant jar
@@ -25,3 +24,11 @@ mkdir /opt/solr-lucene/solr/contrib/uima/lucene-libs
 cd /opt/solr-lucene/solr/contrib/uima/lucene-libs
 ln -s /opt/solr-lucene/lucene/build/analysis/common/lucene-analyzers-common-6.3.0-SNAPSHOT.jar lucene-analyzers-common.jar
 ln -s /opt/solr-lucene/lucene/build/analysis/uima/lucene-analyzers-uima-6.3.0-SNAPSHOT.jar lucene-analyzers-uima.jar
+
+cd /opt/solr-lucene/solr
+/opt/ant/bin/ant server
+chmod +x ./bin/*
+./bin/solr start 
+./bin/solr create -c nutch_core
+./bin/solr create -c mortar_core
+./bin/solr create -c uima_core
